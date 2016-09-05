ef56aadb6bc01783aa58941541308d585097e3ed
Dmitry Polienko <dmitry@eldis.ru>
2016-09-05 11:50:29 +0700

    Restore test_typeclasses for JS

--- tests/boost/test_js.nim
+++ tests/boost/test_js.nim
@@ -1,3 +1,3 @@
-import data.test_stackm, data.test_rbtreem, data.test_rbtree
+import test_typeclasses, data.test_stackm, data.test_rbtreem, data.test_rbtree
 # TODO: fix these for JS
-# import test_typeclasses, test_limits, test_parsers
+# import test_limits, test_parsers
