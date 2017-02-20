de6f4e256f8eecef2ad77175f9752dffa04c6580
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-02-20 15:18:45 +0700

    Add ``genConstructor`` documentation

--- src/boost/objutils.nim
+++ src/boost/objutils.nim
@@ -1,8 +1,8 @@
 import macros
 
-proc generateConstructor(typ: NimNode, n: string, exported: bool): NimNode =
-  expectLen typ.getType, 2
-  let t = typ.getType[1]
+proc generateConstructor(`type`: NimNode, n: string, exported: bool): NimNode =
+  expectLen `type`.getType, 2
+  let t = `type`.getType[1]
   let (obj, isRef) = if t.typeKind == ntyObject:
                        (t.getType, false)
                      elif t.typeKind == ntyRef:
@@ -13,11 +13,11 @@ proc generateConstructor(typ: NimNode, n: string, exported: bool): NimNode =
   let recList = obj[2]
   expectKind recList, nnkRecList
 
-  let name = if n != "": n else: if isRef: "new" & $typ else: "init" & $typ
+  let name = if n != "": n else: if isRef: "new" & $`type` else: "init" & $`type`
   let nameI = if exported: postfix(ident(name), "*") else: ident(name)
 
   var params = newSeq[NimNode]()
-  params.add(typ)
+  params.add(`type`)
   for field in recList:
     params.add(newIdentDefs(ident($field), field.getType))
 
@@ -30,7 +30,22 @@ proc generateConstructor(typ: NimNode, n: string, exported: bool): NimNode =
 
   result = newProc(nameI, params, body)
 
-macro genConstructor*(t: typed, args: varargs[untyped]): untyped =
+macro genConstructor*(`type`: typed, args: varargs[untyped]): untyped =
+  ## Generates constructor for the `type`. The second optional parameter
+  ## is the name of the constructor. By default it's ``newType`` for
+  ## the ref objects and ``initType`` for the objects. The third optional
+  ## parameter is the word ``exported`` and means that the constructor
+  ## must be exported.
+  ##
+  ## Example:
+  ## .. code-block:: nim
+  ##
+  ##   type Obj* = ref object
+  ##     x: int
+  ##     y: string
+  ##   genConstructor Obj, exported
+  ##
+  ##   let x = newObj(1, "a")
   if args.len > 2:
     error("Wrong parameters for constructor generator call")
   let exported = if args.len == 0: false else: $(args[^1]) == "exported"
@@ -40,4 +55,4 @@ macro genConstructor*(t: typed, args: varargs[untyped]): untyped =
              of 0: ""
              else:
                if args.len == 1 and exported: "" else: $args[0]
-  result = generateConstructor(t, name, exported)
+  result = generateConstructor(`type`, name, exported)
