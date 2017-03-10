61cb1a2f9d3a009eb11a4a2e168b8ce99bed5406
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-03-10 18:10:30 +0700

    Add radix to parsers

--- src/boost/parsers.nim
+++ src/boost/parsers.nim
@@ -2,35 +2,9 @@
 
 import limits, strutils
 
-proc strToInt64*(s: string): int64 {.raises: ValueError.} =
-  strutils.parseBiggestInt(s.strip)
+{.push overflowChecks: off.}
 
-proc strToInt*(s: string): int {.raises: ValueError.} =
-  let res = strToInt64(s)
-  when sizeof(int) != 8:
-    if res < int.min.int64 or res > int.max.int64:
-      raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
-  result = res.int
-
-proc strToInt32*(s: string): int32 {.raises: ValueError.} =
-  let res = strToInt64(s)
-  if res < int32.min.int64 or res > int32.max.int64:
-    raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
-  result = res.int32
-
-proc strToInt16*(s: string): int16 {.raises: ValueError.} =
-  let res = strToInt64(s)
-  if res < int16.min.int64 or res > int16.max.int64:
-    raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
-  result = res.int16
-
-proc strToInt8*(s: string): int8 {.raises: ValueError.} =
-  let res = strToInt64(s)
-  if res < int8.min.int64 or res > int8.max.int64:
-    raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
-  result = res.int8
-
-proc strToUInt64*(s: string): uint64 {.raises: ValueError.} =
+proc strToUInt64*(s: string, radix = 10): uint64 {.raises: ValueError.} =
   let d = s.strip
   result = 0
   var prev = 0'u64
@@ -39,37 +13,81 @@ proc strToUInt64*(s: string): uint64 {.raises: ValueError.} =
   if dl > 0 and s[i] == '+':
     inc i
   for i in i..<dl:
-    if d[i] in {'0'..'9'}:
+    if d[i] in {'0'..'9'} or d[i] in {'a'..'z'} or d[i] in {'A'..'Z'}:
+      let n = if d[i] in {'0'..'9'}:
+                ord(d[i]) - ord('0')
+              elif d[i] in {'a'..'z'}:
+                ord(d[i]) - ord('a') + 10
+              else:
+                ord(d[i]) - ord('A') + 10
+      if n >= radix:
+        raise newException(ValueError, "Parse error: " & d & " bad format, radix = " & $radix)
       prev = result
-      result = result * 10 + (ord(d[i]) - ord('0')).uint64
+      result = result * radix.uint64 + n.uint64
       if prev > result:
-        raise newException(ValueError, "Overflow: " & d & " cant fit into uint64")
+        raise newException(ValueError, "Overflow: " & d & " can't fit into uint64")
     elif d[i] == '_':
       continue
     else:
-      raise newException(ValueError, "Parse error: " & d & " cant fit into uint64")
+      raise newException(ValueError, "Parse error: " & d & " bad format")
 
-proc strToUInt*(s: string): uint {.raises: ValueError.} =
-  let res = strToUInt64(s)
+proc strToUInt*(s: string, radix = 10): uint {.raises: ValueError.} =
+  let res = strToUInt64(s, radix)
   when sizeof(uint) != 8:
     if res > uint.max.uint64:
-      raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
+      raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
   result = res.uint
 
-proc strToUInt32*(s: string): uint32 {.raises: ValueError.} =
-  let res = strToUInt64(s)
+proc strToUInt32*(s: string, radix = 10): uint32 {.raises: ValueError.} =
+  let res = strToUInt64(s, radix)
   if res > uint32.max.uint64:
-    raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
+    raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
   result = res.uint32
 
-proc strToUInt16*(s: string): uint16 {.raises: ValueError.} =
-  let res = strToUInt64(s)
+proc strToUInt16*(s: string, radix = 10): uint16 {.raises: ValueError.} =
+  let res = strToUInt64(s, radix)
   if res > uint16.max.uint64:
-    raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
+    raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
   result = res.uint16
 
-proc strToUInt8*(s: string): uint8 {.raises: ValueError.} =
-  let res = strToUInt64(s)
+proc strToUInt8*(s: string, radix = 10): uint8 {.raises: ValueError.} =
+  let res = strToUInt64(s, radix)
   if res > uint8.max.uint64:
-    raise newException(ValueError, "Overflow: " & s & " cant fit into uint")
+    raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
   result = res.uint8
+
+proc strToInt64*(s: string, radix = 10): int64 {.raises: ValueError.} =
+  let d = s.strip
+  var negate = false
+  if d[0] == '-':
+    negate = true
+  result = strToUInt64(if negate: d[1..^1] else: d, radix).int64 * (if negate: -1'i64 else: 1'i64)
+  if negate and result > 0 or (not negate) and result < 0:
+    raise newException(ValueError, "Overflow: " & s & " can't fit into int64")
+
+proc strToInt*(s: string, radix = 10): int {.raises: ValueError.} =
+  let res = strToInt64(s, radix)
+  when sizeof(int) != 8:
+    if res < int.min.int64 or res > int.max.int64:
+      raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
+  result = res.int
+
+proc strToInt32*(s: string, radix = 10): int32 {.raises: ValueError.} =
+  let res = strToInt64(s, radix)
+  if res < int32.min.int64 or res > int32.max.int64:
+    raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
+  result = res.int32
+
+proc strToInt16*(s: string, radix = 10): int16 {.raises: ValueError.} =
+  let res = strToInt64(s, radix)
+  if res < int16.min.int64 or res > int16.max.int64:
+    raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
+  result = res.int16
+
+proc strToInt8*(s: string, radix = 10): int8 {.raises: ValueError.} =
+  let res = strToInt64(s, radix)
+  if res < int8.min.int64 or res > int8.max.int64:
+    raise newException(ValueError, "Overflow: " & s & " can't fit into uint")
+  result = res.int8
+
+{.pop.}
--- tests/boost/test_parsers.nim
+++ tests/boost/test_parsers.nim
@@ -7,8 +7,8 @@ template checkInt(typ: untyped, fun: untyped): untyped =
   check: fun($(typ.max - 77)) == typ.max - 77
   expect(ValueError): discard fun("ZERO")
   when sizeof(typ) != 8 and not defined(js): # Bug https://github.com/nim-lang/Nim/issues/4714
-    expect(ValueError): discard fun($int64.min)
-    expect(ValueError): discard fun($int64.max)
+    expect(ValueError): discard fun($(typ.min.int64 - 1))
+    expect(ValueError): discard fun($(typ.max.int64 + 1))
 
 template checkUInt(typ: untyped, fun: untyped): untyped =
   check: fun($typ.min) == typ.min
@@ -17,7 +17,7 @@ template checkUInt(typ: untyped, fun: untyped): untyped =
   check: fun($(typ.max - 77)) == typ.max - 77
   expect(ValueError): discard fun("ZERO")
   when sizeof(typ) != 8:
-    expect(ValueError): discard fun($int64.max)
+    expect(ValueError): discard fun($(typ.max.int64 + 1))
 
 suite "Parsers":
   test "Ordinal types from string":
@@ -34,3 +34,9 @@ suite "Parsers":
       uint32.checkUInt(strToUInt32)
       uint16.checkUInt(strToUInt16)
       uint8.checkUInt(strToUInt8)
+
+  test "Radix":
+    check: "10".strToInt(10) == 10
+    check: "a".strToInt(16) == 10
+    check: "A".strToInt(16) == 10
+    check: "deadbeef".strToInt(16) == 0xdeadbeef
